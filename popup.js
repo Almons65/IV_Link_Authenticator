7f24d7c2697c80ec06282c380f942386dd9a5298
Adisorn <adisorn.almond@gmail.com>
2026-01-06 15:40:42 +0700

    Initial commit

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        let curTab = tabs[0];
+        let curUrl = currentTab.url;
+        
+        document.getR
+    })}) 
